6998e41babe7fdcdfc47388ec3c4ee74dcce2fed
mihnpro <mihnpro@gmail.com>
2026-06-04 14:38:47 +0300

    Keep only 1 neccesery method

--- services/gateway/internal/app/port/account.go
+++ services/gateway/internal/app/port/account.go
@@ -1,8 +1,5 @@
 package port
 
-import "github.com/mihnpro/Merch_shop/services/gateway/internal/domain"
-
 type Verifier interface {
-	VerifyAccess(token string) (*domain.Claims, error)
-	VerifyRefresh(token string) (*domain.Claims, error)
+	VerifyAccess(token string) error
 }
